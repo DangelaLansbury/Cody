52720574efc9f763ea60f7c9407e34b11b0a456f
DangelaLansbury <42824545+DangelaLansbury@users.noreply.github.com>
2024-10-14 23:50:59 -0400

    Test imports

---
+++ src/components/WheelGraphic.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  background-color: red;
+  display: block;
+  height: 80px;
+  width: 80px;
+`;
+
+function WheelGraphic() {
+  return <StyledDiv />;
+}
+
+export default WheelGraphic;
